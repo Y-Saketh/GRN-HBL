57aaa4122cc15d73a2ddc65e02e7b90221b43f8d
sharviinfotech <sharviinfotech@gmail.com>
2024-12-09 13:21:01 +0530

    added path

--- src/app/pages/dashboards/dashboards-routing.module.ts
+++ src/app/pages/dashboards/dashboards-routing.module.ts
@@ -11,9 +11,10 @@ import { QRcodegenrationComponent } from 'src/app/GRNQRCodeGeneration/qrcodegenr
 import { GrpendingComponent } from 'src/app/GRNQRCodeGeneration/grpending/grpending.component';
 import { GrdoneComponent } from 'src/app/GRNQRCodeGeneration/grdone/grdone.component';
 import { CloseDownloadComponent } from 'src/app/GRNQRCodeGeneration/close-download/close-download.component';
-import { Mb51Component } from 'src/app/GRNQRCodeGeneration/mb51/mb51.component';
+// import { Mb51Component } from 'src/app/GRNQRCodeGeneration/mb51/mb51.component';
 import { Mb52Component } from 'src/app/GRNQRCodeGeneration/mb52/mb52.component';
 import { GrnprintComponent } from 'src/app/GRNQRCodeGeneration/grnprint/grnprint.component';
+import { Mb51Component } from 'src/app/GRNQRCodeGeneration/mb51/mb51.component';
 
 
 const routes: Routes = [
