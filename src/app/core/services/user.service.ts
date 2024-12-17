ba486fe71547b91cacb6a2cbb388c563343e30ee
Sharvi Infotech <sharviinfotech@gmail.com>
2024-12-17 14:10:26 +0530

    merged

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -91,10 +91,4 @@ export class UserProfileService {
     }
 
 
-    fetchMb52Data(data){
-        console.log("environment.API_URL_DEV",environment.API_URL_DEV)
-        return this.http.post(`${environment.API_URL_DEV}api/external/mb52`,data)
-    }
-
-
 }
