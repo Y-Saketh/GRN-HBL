675bfb259f8db1343b6e464cd512c21959be533d
Sharvi Infotech <sharviinfotech@gmail.com>
2024-12-30 12:20:40 +0530

    added format

--- src/app/GRNQRCodeGeneration/goods-returns/goods-returns.component.ts
+++ src/app/GRNQRCodeGeneration/goods-returns/goods-returns.component.ts
@@ -150,7 +150,7 @@ export class GoodsReturnsComponent {
               "MBLNR": this.materialDocument,
               "MJAHR": this.year,
               "BUDAT": this.postingDate,
-              "BLDAT":  moment(this.documentDate).format('YYYY-MM-DD'),//this.documentDate,
+              "BLDAT": moment(this.documentDate, 'DD-MM-YYYY').toISOString(),//this.documentDate,
               "BKTXT": this.form.headerText.value,
             },
             ITEM: []
