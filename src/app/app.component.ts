c729242f0e078c02360389c18527cc64dde85074
Sharvi Infotech <sharviinfotech@gmail.com>
2025-01-03 15:50:22 +0530

    the resolution has reduced to 80%

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoaderService } from './core/services/loader.service';
 import { SessionServiceService } from './shared/ui/session-service.service';
@@ -12,11 +12,17 @@ import { SessionServiceService } from './shared/ui/session-service.service';
   imports: [RouterOutlet,CommonModule],
 })
 export class AppComponent implements OnInit {
+ 
 
   ngOnInit() {
 
     console.log("appplication started")
+    this.setBrowserZoom(80);
     
   }
-  constructor(public loaderService: LoaderService, private sessionService:SessionServiceService) {}
+  constructor(public loaderService: LoaderService, private sessionService:SessionServiceService,private renderer: Renderer2) {}
+  setBrowserZoom(percentage: number): void {
+    const zoomLevel = percentage / 100;
+    this.renderer.setStyle(document.body, 'zoom', zoomLevel.toString());
+  }
 }
