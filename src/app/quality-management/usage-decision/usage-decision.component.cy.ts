57fc6d9b04da6b74c479a31b8ad2a5c152e006e0
sharviinfotech <sharviinfotech@gmail.com>
2025-01-10 11:18:56 +0530

    deleted unwanted files in ud screen

--- src/app/quality-management/usage-decision/usage-decision.component.cy.ts
+++
@@ -1,7 +0,0 @@
-import { UsageDecisionComponent } from './usage-decision.component'
-
-describe('ResultRecordingComponent', () => {
-  it('should mount', () => {
-    cy.mount(ResultRecordingComponent)
-  })
-})
