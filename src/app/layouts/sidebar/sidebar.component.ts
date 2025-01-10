10da8a58081eb2173e1e3137b14a2f07f2a0c117
Sharvi Infotech <sharviinfotech@gmail.com>
2025-01-10 13:22:55 +0530

    added the APi changes

--- src/app/layouts/sidebar/sidebar.component.ts
+++ src/app/layouts/sidebar/sidebar.component.ts
@@ -157,7 +157,12 @@ export class SidebarComponent implements OnInit, AfterViewInit, OnChanges {
     }else if (loginResponse && loginResponse[0].ZQMACT && loginResponse[0].ZQMACT.ZQMACT1 == "QM" && project == "qm" ) {
       const authorizedIds: string[] = Object.values(loginResponse[0].ZQMACT).filter((id): id is string => typeof id === 'string' && id.trim() !== "");
       this.menuItems = this.filterMenuItems(MENU, authorizedIds);
-    } else {
+    } 
+    //  if (loginResponse && loginResponse[0].ZGRNACT ) {
+    //   const authorizedIds: string[] = Object.values(loginResponse[0].ZGRNACT).filter((id): id is string => typeof id === 'string' && id.trim() !== "");
+    //   this.menuItems = this.filterMenuItems(MENU, authorizedIds);
+    // } 
+    else {
       this.menuItems = []; // Fallback if no loginResponse or ZGRNACT
     }
   }
